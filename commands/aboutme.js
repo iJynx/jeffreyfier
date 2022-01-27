8b2312cacbb3d03986f7aa1f5a4a2a5c33f2124e
Swatarianess <swatarianess@gmail.com>
2022-01-27 20:57:33 +0100

    code cleanup

--- commands/aboutme.js
+++ commands/aboutme.js
@@ -1,4 +1,5 @@
 const { codeBlock } = require("@discordjs/builders");
+const logger = require("../modules/Logger");
 
 exports.run = async (client, interaction) => { // eslint-disable-line no-unused-vars
   const info = codeBlock("asciidoc", `
@@ -8,10 +9,7 @@ exports.run = async (client, interaction) => { // eslint-disable-line no-unused-
   
   Disciples will then see the most controversial messages and decide if the Jeffrey vote was fair or not. You get three strikes being a Jeffrey and then it's a ban.
 `);
-  user_person = interaction.author;
-  await user_person.send(info);
-  console.log(`DM'd aboutme to: ${user_person}`);
-  // await interaction.author.reply(info);
+  await interaction.author.send(info);
 };
 
 
